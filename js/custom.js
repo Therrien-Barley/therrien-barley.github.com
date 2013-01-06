dd001a042da38acaac7c66df21dc575451ae753f
Troy Conrad Therrien <troyth@gmail.com>
2013-01-05 23:12:42 -0500

    ip

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,10 @@
 jQuery(document).ready(function() {
 
 
-	$('#fixed-header-left').affix();
-	$('#fixed-header-right').affix();
-	$('#fixed-logo-container').affix();
+	//$('#fixed-header-left').affix();
+	//$('#fixed-header-right').affix();
+	//$('#fixed-logo-container').affix();
+	$('#header-nav').affix();
 	
 	$('#gf').text('GitHub Followers');
     $('#gfr').text('GitHub Repos');		
